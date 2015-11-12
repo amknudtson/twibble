cbe2db16064e5a428e024b55c1f27687357634bd
Jazmyn R <jazmyn.russell@gmail.com>
2015-11-11 20:13:38 -0600

    Adds Vishesh's edits
    This is a janky way to merge Vishesh’s pull request because I tried
    (and failed) to resolve the conflicts so I just did this instead. This
    is bad practice in action.

--- read_data.py
+++ read_data.py
@@ -1,7 +1,6 @@
 
 import json
 import pandas as pd 
-import matplotlib.pyplot as plt 
 
 tweets_data_path='twitter_data.txt' 
 
@@ -16,29 +15,36 @@ for line in tweets_file:
 
 print len(tweets_data) 
 
-tweets=pd.DataFrame()
+# tweets=pd.DataFrame()
 
-tweets['text']=map(lambda tweet: tweet['text'],tweets_data)
-tweets['lang']=map(lambda tweet: tweet['lang'], tweets_data)
-tweets['country']=map(lambda tweet: tweet['place']['country'] if tweet['place']!=None else None, tweets_data)
+with open("readed.csv", "writer") as csvfile:
+	writer = csv.writer(csvfile)
+	for twit in tweets_data:
+		a = twit['text'].encode('utf-8')
+		writer.writerow([str(a),])
 
-tweets_by_lang=tweets['lang'].value_counts()
 
-fig, ax=plt.subplots()
-ax.tick_params(axis='x', labelsize=15)
-ax.tick_params(axis='y', labelsize=10)
-ax.set_xlabel('Languages', fontsize=15)
-ax.set_ylabel('Numer of tweets', fontsize=15)
-ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
-tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+# tweets['text']=map(lambda tweet: tweet['text'],tweets_data)
+# tweets['lang']=map(lambda tweet: tweet['lang'], tweets_data)
+# tweets['country']=map(lambda tweet: tweet['place']['country'] if tweet['place']!=None else None, tweets_data)
 
-tweets_by_country=tweets['country'].value_counts()
+# tweets_by_lang=tweets['lang'].value_counts()
 
-fig, ax=plt.subplots()
-ax.tick_params(axis='x',labelsize=15)
-ax.tick_params(axis='y',labelsize=10)
-ax.set_xlabel('Countries', fontsize=15)
-ax.set_ylabel('Number of tweets', fontsize=15)
-ax.set_title('Top 5 Countries', fontsize=15, fontweight='bold')
-tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
+# fig, ax=plt.subplots()
+# ax.tick_params(axis='x', labelsize=15)
+# ax.tick_params(axis='y', labelsize=10)
+# ax.set_xlabel('Languages', fontsize=15)
+# ax.set_ylabel('Numer of tweets', fontsize=15)
+# ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+# tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+
+# tweets_by_country=tweets['country'].value_counts()
+
+# fig, ax=plt.subplots()
+# ax.tick_params(axis='x',labelsize=15)
+# ax.tick_params(axis='y',labelsize=10)
+# ax.set_xlabel('Countries', fontsize=15)
+# ax.set_ylabel('Number of tweets', fontsize=15)
+# ax.set_title('Top 5 Countries', fontsize=15, fontweight='bold')
+# tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
 
