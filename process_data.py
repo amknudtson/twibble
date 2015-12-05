eae364d270a9841c4331d3851a56e8ccf1edc70f
amknudtson <amknudtson@wisc.edu>
2015-12-05 15:02:41 -0600

    Enhanced emotion processing
    Synonyms, punctuation splitting, etc.

--- process_data.py
+++ process_data.py
@@ -1,81 +1,71 @@
+import re
 import csv
 
 data = open("readed.csv", "r")
 tweets = []
 
 for twit in data:
-	tweets.append(str(twit).strip())
+  tweets.append(str(twit).strip())
 
 # print tweets
 
-def findWord(w):
-  if (re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search == None):
-    return True
-  else:
-    return False
+happySyn = ['happy', 'glad', 'excited', ':)', 'love', ':-))','great', 'lol', 'ecstatic', 'thrilled', ';)', 'gleeful', ':d', 'joyful', 'yay', 'hooray', ':-)', ':o)', '=)', '\o/','xd' ]
+sadSyn = ['sad', 'depressed', 'fml', 'worry', 'worried', 'sorry', 'somber', 'crying', 'heartbroken', 'alone', 'bummed', ':(', 'blah', 'meh', 'downer', ':-(', '</3']
+angrySyn = ['angry', 'pissed', 'mad', '>:(', 'petulance', '>.<', 'upset', 'furious', 'outraged', 'rage', 'outrage', 'irritated', 'irritable', 'vexed', 'tantrum', 'hissy', 'aggravated', 'annoyed', 'ballistic', 'ticked' ]
 
-def tweetListHappy():
- happyArray = []
- for i in tweets:
-   # tweetlist = i.split()
-   if 'happy' in tweetlist:
-     happyArray.append(1)
-   else:
-     happyArray.append(0)
- return happyArray
- 
-# This is identical to tweetListHappy(), except the function evaluates whether each tweet contains the word "sad" or not.  
-def tweetListSad():
- sadArray = []
- for i in tweets:
-   tweetlist = i.split()
-   if 'sad' in tweetlist:
-     sadArray.append(1)
-   else: 
-     sadArray.append(0)
- return sadArray
+# Each individual tweet in a given array is broken into another array made of a list of words contained in that tweet.  Another array is then created for the set of tweets as a
+# whole which looks at each individual tweet array and assigns a value of 0 or 1 for that tweet.  A 0 is" assigned if the individual tweet contains a specified word ("happy").
+# A 1 is assigned if the tweet does not contain that word.  This is done for all tweets in the original array.  The resulting array is the returned value.  
 
-# This is identical to tweetListHappy(), except the function evaluates whether each tweet contains the word "angry" or not.   
-def tweetListAngry():
- angryArray = []
- for i in tweets:
-   tweetlist = i.split()
-   if 'angry' in tweetlist:
-     angryArray.append(1)
-   else:
-     angryArray.append(0)
- return angryArray
+def tweetListFilter(_tweets, _emotions):
+  emotionArray=[]
+  for tweet in _tweets:
+    tweetlower=tweet.lower()
+    wordsintweetlower = re.split('[, | |. |! |\? |\"]',tweetlower)  #splits tweets based on designated characters; '; |, | |. |! |: |\? |\"'
+    for i in _emotions: 
+      if i in wordsintweetlower: 
+        synIndex = wordsintweetlower.index(i)  #gets the indexposition of the syn word in the array of the tweet
+        if synIndex > 0:
+          before = wordsintweetlower[synIndex - 1]  #get the word in index position of the word before the syn word
+          checkNot = before[-3:] #sets variable to last three letters of the word before our syn
+          if checkNot == "not":  #  checks if word before syn is "not", or ends in "not";  note:  this ignores a handful of english words that end in "not" (ex. knot).  Determined missing results would be minimal
+            emotionArray.append(0)
+            break
+          elif checkNot == "n't":  # checks if word before syn ends in "n't"
+            emotionArray.append(0)
+            break
+          else:
+            emotionArray.append(1) # if syn not preceded by "not" or "n't", then appends 1 to emotionsArray
+            break
+        else:
+          emotionArray.append(1)  # if syn present as first word, then appends 1 to emotionsArray
+          break
+      else: 
+        emotionArray.append(0)  # if syn not present, appends 0 to emotionsArray
+  sumEmotion = 0
+  for i in emotionArray:  # Counts total number of 1s in emotionsArray to determine total number of applicable tweets for emotion
+    sumEmotion += i
+  return sumEmotion
 
-# This function adds the values of the indices in tweetListHappy() and returns the number of tweets containing the word "happy".
-def sumHappy():
-  happyArray = tweetListHappy()
-  sumHappy = 0
-  for i in happyArray:
-    sumHappy = sumHappy + i
-  return sumHappy
-
-# This function adds the values of the indices in tweetListSad() and returns the number of tweets containing the word "sad".
-def sumSad():
-  sadArray = tweetListSad()
-  sumSad = 0
-  for i in sadArray:
-    sumSad = sumSad + i
-  return sumSad
-
-# This function adds the values of the indices in tweetListAngry() and returns the number of tweets containing the word "angry".
-def sumAngry():
-  angryArray = tweetListAngry()
-  sumAngry = 0
-  for i in angryArray:
-    sumAngry = sumAngry + i
-  return sumAngry 
 
 # This function prints lists describing how many "happy", "sad", and "angry" tweets were present in the data set.  A tweet may be counted in one, two, or all three lists, 
 # or in no lists.  Each tweet is only counted once per list type. 
-def emotionsCountList():
-  happy = sumHappy()
-  sad = sumSad()
-  angry = sumAngry()
+def display():
+  happy = tweetListFilter(tweets, happySyn)
+  sad = tweetListFilter(tweets, sadSyn)
+  angry = tweetListFilter(tweets, angrySyn)
   print "Number of happy tweets: " + str(happy)
   print "Number of sad tweets: " + str(sad)
-  print "Number of angry tweets: " + str(angry)
+  print "Number of angry tweets: " + str(angry) + "\n"
+  
+  totalCount = happy + sad + angry
+  
+  happyFraction = float(happy)/float(totalCount)
+  sadFraction = float(sad)/float(totalCount)
+  angryFraction = float(angry)/float(totalCount)
+
+  print "Happy fraction:" + str(happyFraction)
+  print "Sad fraction:" + str(sadFraction)
+  print "Angry fraction:" + str(angryFraction) + "\n"
+
+display()
